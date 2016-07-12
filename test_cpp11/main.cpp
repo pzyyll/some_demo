abb19ec3e319f65e7fe5761266c051b3f007cde5
caizhili <pzyyll@gmail.com>
2016-07-12 15:13:30 +0800

    Alter c++ use using replace typedef

--- test_cpp11/main.cpp
+++ test_cpp11/main.cpp
@@ -2,7 +2,17 @@
 
 using namespace std;
 
+//c++ using == typedef;
+struct DemoClass {
+    int a_mem;
+};
+
+using DC = DemoClass;
+
 int main() {
-    cout << "Hello, World!" << endl;
+    DC a_demo_var;
+    a_demo_var.a_mem = 10;
+
+    cout << a_demo_var.a_mem << endl;
     return 0;
 }
