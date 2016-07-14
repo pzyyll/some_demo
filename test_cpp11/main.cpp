3bafbb3aabd8571f99a3d456b51eb46dd2898efc
caizhili <pzyyll@gmail.com>
2016-07-14 11:50:27 +0800

    decltype test

--- test_cpp11/main.cpp
+++ test_cpp11/main.cpp
@@ -9,10 +9,24 @@ struct DemoClass {
 
 using DC = DemoClass;
 
+int Size()
+{
+    return 4;
+}
+
 int main() {
     DC a_demo_var;
     a_demo_var.a_mem = 10;
 
     cout << a_demo_var.a_mem << endl;
+
+    decltype(a_demo_var) xt;
+    xt.a_mem = 2;
+
+    cout << xt.a_mem << endl;
+
+    decltype(Size()) type_from_size = 4;
+    cout << type_from_size << endl;
+
     return 0;
 }
